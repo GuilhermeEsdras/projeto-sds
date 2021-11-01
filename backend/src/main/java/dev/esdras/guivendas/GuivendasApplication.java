e2b7e4fb900656c0ccbe775b95ceed0edbca42b6
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2021-11-01 18:56:17 -0300

    First commit :sparkles:

---
+++ backend/src/main/java/dev/esdras/guivendas/GuivendasApplication.java
@@ -0,0 +1,13 @@
+package dev.esdras.guivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuivendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuivendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/esdras/guivendas/GuivendasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.esdras.guivendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuivendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
