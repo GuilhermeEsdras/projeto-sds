21284df334457e99bf1cd60b2e9a6bbd0b87553e
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2021-11-01 21:08:24 -0300

    Front End - Sass Folder Structuring + Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/sass/styles.scss';
+
 import App from './App';
 
 ReactDOM.render(
