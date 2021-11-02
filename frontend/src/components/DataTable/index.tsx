71b601036745497f15759e3b1fba52267f9e187e
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2021-11-01 22:03:01 -0300

    Front End - Basic static components + Styles Folder adjustments + gui esdras logo

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,49 @@
+const DataTable = () => {
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">Data</th>
+            <th scope="col">Vendedor</th>
+            <th scope="col">Clientes visitados</th>
+            <th scope="col">Negócios Fechados</th>
+            <th scope="col">Valor</th>
+          </tr>
+        </thead>
+        <tbody className="table-hover">
+          <tr>
+            <td>22/04/2021</td>
+            <td>Guilherme Esdras</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Guilherme Esdras</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Guilherme Esdras</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Guilherme Esdras</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
