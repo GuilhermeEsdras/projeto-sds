33531aaf5f9a737e551cc043d21d90c3af0ee0a3
Guilherme Esdras <guilherme.esdras@academico.ifpb.edu.br>
2021-11-03 15:24:06 -0300

    Front End - Table redesign by Gui

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,47 +1,81 @@
-const DataTable = () => {
+interface TableRowProps {
+  data: string;
+  vendedor: string;
+  clientesVisitados: number;
+  negociosFechados: number;
+  valor: string;
+}
+
+const TableRow = ({
+  data,
+  vendedor,
+  clientesVisitados,
+  negociosFechados,
+  valor,
+}: TableRowProps) => {
   return (
-    <div className="table-responsive mb-5">
-      <table className="table table-striped table-sm table-hover">
-        <thead className="table-light">
-          <tr>
-            <th scope="col">Data</th>
-            <th scope="col">Vendedor</th>
-            <th scope="col">Clientes visitados</th>
-            <th scope="col">Negócios Fechados</th>
-            <th scope="col">Valor</th>
-          </tr>
-        </thead>
-        <tbody className="table-hover">
-          <tr>
-            <td>22/04/2021</td>
-            <td>Guilherme Esdras</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Guilherme Esdras</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Guilherme Esdras</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Guilherme Esdras</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-        </tbody>
-      </table>
+    <tr className="gui-table-row">
+      <td className="gui-table-column gui-column1">{data}</td>
+      <td className="gui-table-column gui-column2">{vendedor}</td>
+      <td className="gui-table-column gui-column3">{clientesVisitados}</td>
+      <td className="gui-table-column gui-column4">{negociosFechados}</td>
+      <td className="gui-table-column gui-column5">{valor}</td>
+    </tr>
+  );
+};
+
+const DataTable: React.FC = () => {
+  return (
+    <div className="gui-table-container mb-5">
+      <div className="gui-table-wrapper">
+        <div className="gui-table-div gui-table">
+          <table data-vertable="gui-table" className="gui-table">
+            <thead>
+              <tr className="gui-table-row gui-table-head">
+                <th className="gui-table-column gui-column1">Data</th>
+                <th className="gui-table-column gui-column2">Vendedor</th>
+                <th className="gui-table-column gui-column3">
+                  Clientes visitados
+                </th>
+                <th className="gui-table-column gui-column4">
+                  Negócios Fechados
+                </th>
+                <th className="gui-table-column gui-column5">Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TableRow
+                data="22/04/2021"
+                vendedor="Guilherme Esdras"
+                clientesVisitados={34}
+                negociosFechados={25}
+                valor="15017.00"
+              />
+              <TableRow
+                data="22/04/2021"
+                vendedor="Guilherme Esdras"
+                clientesVisitados={34}
+                negociosFechados={25}
+                valor="15017.00"
+              />
+              <TableRow
+                data="22/04/2021"
+                vendedor="Guilherme Esdras"
+                clientesVisitados={34}
+                negociosFechados={25}
+                valor="15017.00"
+              />
+              <TableRow
+                data="22/04/2021"
+                vendedor="Guilherme Esdras"
+                clientesVisitados={34}
+                negociosFechados={25}
+                valor="15017.00"
+              />
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
